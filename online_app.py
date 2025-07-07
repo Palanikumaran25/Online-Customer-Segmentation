70cf619ab4152f18c40db759a74bf1f139b43e1a
Palanikumaran K <palanikumaran@thirumoolarsoft.com>
2025-07-07 10:38:33 +0530

    online_app.py

--- online_app.py
+++ online_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
 from datetime import timedelta
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
